999aef1eeaa536960e6a68d3e02254dc02c99afa
Rajib Sharia <rajibsharia@gmail.com>
2026-02-11 15:13:05 +0800

    cyberpanel core database structure and installation script

---
+++ restore_cyberpanel_database.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## Download prototype cyberpanel database
+#(curl https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cyberpanel.sql -o cyberpanel.sql || wget -q https://raw.githubusercontent.com/tbaldur/cyberpanel-mods/main/cyberpanel.sql)
+#Read mysql password
+mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
+
+#Creates empty cyberpanel database
+mysql -u root -p"$mysql_password" cyberpanel -e "CREATE DATABASE IF NOT EXISTS cyberpanel;"
+
+#Populates cyberpanel database with prototype
+mysql -u root -p"$mysql_password" cyberpanel < cyberpanel.sql
+
+#Deletes cyberpanel.sql
+#rm -f cyberpanel.sql
+
+#Restart cyberpanel
+systemctl restart lscpd
+
+#Generate new random cyberpanel password
+newPassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1)
+adminPass "$newPassword"
+echo "Your cyberpanel password is: $newPassword"
